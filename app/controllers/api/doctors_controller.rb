774c88558f5a60a82b45bebaf68559d5152d3eff
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-08 16:52:52 -0700

    doctors and patients controllers done

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,2 +1,36 @@
 class Api::DoctorsController < ApplicationController
+   
+    before_action :set_doctor, only: [:show, :destroy]
+    
+    def index
+        render json: Doctor.all, include: [:appointments, :patients]
+    end
+
+    def show
+        render json: @doctor, include: [:appointments, :patients]
+    end
+
+    def create
+        @doctor = Doctor.new(doc_params)
+        if @doctor.save
+            render json: @doctor, include: [:appointments, :patients]
+        else
+            render json: {error: @doctor.errors}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @doctor.destroy
+    end
+
+    private
+
+    def set_doctor
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def doc_params
+        params.require(:doctor).permit(:name)
+    end
+
 end
--- app/controllers/api/patients_controller.rb
+++ app/controllers/api/patients_controller.rb
@@ -1,2 +1,36 @@
 class Api::PatientsController < ApplicationController
+
+    before_action :set_patient, only: [:show, :destroy]
+    
+    def index
+        render json: Patient.all, include: [:appointments, :doctors]
+    end
+
+    def show
+        render json: @patient, include: [:appointments, :doctors]
+    end
+
+    def create
+        @patient = Patient.new(patient_params)
+        if @patient.save
+            render json: @patient, include: [:appointments, :doctors]
+        else
+            render json: {error: @patient.errors}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @patient.destroy
+    end
+
+    private
+
+    def set_patient
+        @patient = Patient.find(params[:id])
+    end
+
+    def patient_params
+        params.require(:patient).permit(:name)
+    end
+
 end
