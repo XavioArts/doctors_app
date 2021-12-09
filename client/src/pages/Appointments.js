45acbe61bfc75336da369480202b8ef0b939c3e1
Asher Bay <asherbay@gmail.com>
2021-12-09 14:34:03 -0700

    started appointments.js

--- client/src/pages/Appointments.js
+++ client/src/pages/Appointments.js
@@ -1,6 +1,12 @@
 import React from "react";
+import useAxiosOnMount from "../hooks/useAxiosOnMount";
 
 const Appointments = () => {
+    const {
+        data: appointments,
+        error: appointmentsError,
+        setData: setAppointments,
+      } = useAxiosOnMount("/api/grades");
     return (
         <div>
             <h1>Appointments page</h1>
