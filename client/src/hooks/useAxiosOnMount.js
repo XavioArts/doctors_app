d9771fdad908f5c374be3729ac699290c01a5302
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-09 14:31:47 -0700

    updated axios on mount

--- client/src/hooks/useAxiosOnMount.js
+++ client/src/hooks/useAxiosOnMount.js
@@ -4,22 +4,26 @@ import axios from "axios";
 const useAxiosOnMount = (url) => {
     const[data, setData] = useState(null);
     const[error, setError] = useState(null);
+    const[loading, setLoading] = useState(true);
     
     useEffect(()=> {
         getData();
     }, []);
 
     const getData = async () => {
+        setLoading(true);
         try {
             let res = await axios.get(url);
             setData(res.data);
+            setLoading(false);
         } catch {
             setError("error occured");
+            setLoading(false);
             // addd a better error message
         }
     };
 
-    return { data, error }
+    return { data, error, setData, loading };
 
 };
 
