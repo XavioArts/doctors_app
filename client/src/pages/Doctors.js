13366475a311b1393e1c238a1db4a8a4346084a0
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-09 17:24:59 -0700

    doctors page layout done

--- client/src/pages/Doctors.js
+++ client/src/pages/Doctors.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { List } from "semantic-ui-react";
+import { Button, List } from "semantic-ui-react";
 import DoctorsContainer from "../components/DoctorsContainer";
 import SemanticLoader from "../components/SemanticLoader";
 import useAxiosOnMount from "../hooks/useAxiosOnMount";
@@ -12,9 +12,12 @@ const Doctors = () => {
         return doctors.map((doctor) => {
             return (
                 <List.Item>
+                    <List.Content floated='right'>
+                        <Button>Select</Button>
+                    </List.Content>
                   <List.Icon name='user md' size='large' verticalAlign='middle' />
                   <List.Content>
-                    <List.Header as='a'>{doctor.name}</List.Header>
+                    <List.Header as='h4'>{doctor.name}</List.Header>
                     <List.Description as='p'>Total Appointments: {doctor.appointments.length}</List.Description>
                   </List.Content>
                 </List.Item>
@@ -26,44 +29,9 @@ const Doctors = () => {
         <DoctorsContainer>
             <h1>Doctors page</h1>
             {loading && <SemanticLoader text="Getting doctors..." />}
-            {doctors && <List divided relaxed>{renderDoctors()}</List>}
+            {doctors && <List divided relaxed size="large" style={{width: "75vw"}}>{renderDoctors()}</List>}
         </DoctorsContainer>
     );
-
-
-        // const [doctors, setDoctors] = useState([]);
-
-    // useEffect(() => {
-    //     getDoctors();
-    // }, []);
-
-    // const getDoctors = async () => {
-    //     let res = await axios.get("/api/doctors");
-    //     setDoctors(res.data);
-    // }
-    // const renderDoctors = () => {
-    //     return doctors.map((doctor) => {
-    //         const renderApps = () => {
-    //             return doctor.appointments.map((app) => {
-    //                 return <p>Appointment date: {app.date}, with {app.patient_id}</p>;
-    //             });
-    //         };
-    //         const renderPatients = () => {
-    //             return doctor.patients.map((p) => {
-    //                 return <p>Name: {p.name}, Patient Id: {p.id}</p>;
-    //             });
-    //         };
-    //         return (
-    //             <div>
-    //                 <h2>{doctor.name}</h2>
-    //                 <h4>Appointments:</h4>
-    //                 {renderApps()}
-    //                 <h4>Patients:</h4>
-    //                 {renderPatients()}
-    //             </div>
-    //         );
-    //     });
-    // };
 };
 
 export default Doctors;
