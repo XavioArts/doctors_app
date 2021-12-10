5e60d4dd10c2443a954016d760235f958858f07d
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-10 14:41:51 -0700

    new doctor form done

---
+++ client/src/components/Styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CenterDiv = styled.div`
+    padding: 20px;
+    text-align: center;
+    margin: auto;
+`;
--- client/src/pages/Doctors.js
+++ client/src/pages/Doctors.js
@@ -1,12 +1,25 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Button, List } from "semantic-ui-react";
+import { Button, List, Segment } from "semantic-ui-react";
+import DoctorNewForm from "../components/DoctorNewForm";
 import DoctorsContainer from "../components/DoctorsContainer";
 import SemanticLoader from "../components/SemanticLoader";
+import { CenterDiv } from "../components/Styles";
 import useAxiosOnMount from "../hooks/useAxiosOnMount";
 
 const Doctors = () => {
+    const [show, setShow] = useState(false);
+    const [active, setActive] = useState(false);
     const { data: doctors, setData: setDoctors, loading, error } = useAxiosOnMount("/api/doctors");
+
+    const toggleShow = () => {
+        setShow(!show);
+        setActive(!active);
+    };
+
+    const addDoctor = (doc) => {
+        setDoctors([...doctors, doc]);
+    };
 
     const renderDoctors = () => {
         return doctors.map((doctor) => {
@@ -27,9 +40,16 @@ const Doctors = () => {
 
     return (
         <DoctorsContainer>
-            <h1>Doctors page</h1>
+            <h1>Doctors Portal</h1>
+            <h4>Welcome, please select your doctor.</h4>
             {loading && <SemanticLoader text="Getting doctors..." />}
             {doctors && <List divided relaxed size="large" style={{width: "75vw"}}>{renderDoctors()}</List>}
+            <h4>Not you?</h4>
+            <p>Create a new doctor profile</p>
+            <Segment as={CenterDiv} style={{width: "55vw"}}>
+                <Button toggle active={active} onClick={toggleShow} >New</Button>
+                {show && <DoctorNewForm addDoctor={addDoctor} toggleShow={toggleShow} />}
+            </Segment>
         </DoctorsContainer>
     );
 };
