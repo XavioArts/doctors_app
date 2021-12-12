a14add602336319932c53970111633ce0edeef3b
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-12 00:58:05 -0700

    doctor show working with all data/ layout done

--- client/src/pages/DoctorShow.js
+++ client/src/pages/DoctorShow.js
@@ -1,41 +1,101 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router";
+import React from "react";
+import { useNavigate, useParams } from "react-router";
 import DoctorsContainer from "../components/DoctorsContainer";
 import SemanticLoader from "../components/SemanticLoader";
 import useAxiosOnMount from "../hooks/useAxiosOnMount";
-import { Button, Divider, Icon } from "semantic-ui-react";
-import { FlexDiv } from "../components/Styles";
+import { Button, Divider, Icon, Segment, List } from "semantic-ui-react";
+import { CenterDiv, FlexDiv } from "../components/Styles";
+
 
 const DoctorShow = () => {
 
     const {id} = useParams();
-    // const [doctor, setDoctor] = useState(null);
-    // const [patients, setPatients] = useState([]);
+    const navigate = useNavigate();
+    
     const { data: doctor, setData: setDoctor, loading: doc_loading, error: doc_error } = useAxiosOnMount(`/api/doctors/${id}`);
-    const { data: patients, setData: setPatients, loading: patients_loading, error } = useAxiosOnMount(`/api/patients`);
+    const { data: patients, setData: setPatients, loading: patients_loading, error: patients_error } = useAxiosOnMount(`/api/patients`);
+    const { data: appointments, setData: setAppointments, loading: appointments_loading, error } = useAxiosOnMount(`/api/appointments`);
+  
 
+    const filterApps = () => {
+        let apps = appointments.filter((a) => a.doctor_id === doctor.id);
+        return apps;
+    };
 
+    const filterPat = () => {
+        let apps = filterApps();
+        // console.log(apps);
+        // console.log(patients);
+        let filter = patients.filter((p) => apps.find((a)=>a.patient_id === p.id));
+        return filter;
+    };
+
+    const renderAppointments = () => {
+        const docApps = filterApps();
+        return docApps.map((a)=> {
+            return (
+                <List.Item key={a.id}>
+                    <List.Content floated='right'>
+                        <Button onClick={()=>navigate(`/doctors/${doctor.id}/appointments/${a.id}`)}>Select</Button>
+                    </List.Content>
+                    <List.Content>
+                        <List.Header>{a.date}</List.Header>
+                        <List.Description>with {a.patient_name}</List.Description>
+                    </List.Content>
+                </List.Item>
+            );
+        });
+    };
+
+    const renderPatients = () => {
+        const docPatients = filterPat();
+        // console.log(docPatients);
+        return docPatients.map((p) => {
+            return (    
+                <List.Item key={p.id}>
+                    <List.Content>
+                        <List.Header>{p.name}</List.Header>
+                    </List.Content>
+                </List.Item>
+            );
+        });
+    };
+
+    if (!doctor || !patients || !appointments) {
+        return <SemanticLoader />
+    }
 
     return (
         <DoctorsContainer>
-            {doc_loading && patients_loading && <SemanticLoader />}
-            <FlexDiv>
-                <Icon name="user circle" size="massive" />
-                {doctor && <h1>{doctor.name}</h1>}
-            </FlexDiv>
-            <Divider />
-            <Button.Group>
-                <Button>Edit</Button>
-                <Button color="red">Delete</Button>
-            </Button.Group>
-            <Divider />
-            <h2>Current Appointments</h2>
-            <p>appintments will go here</p>
-            <Button>Add Appointment</Button>
-            {/* form will go here toggled */}
-            <h2>Patients</h2>
-            <p>Patienst will go here</p>
+            {doc_loading && patients_loading && appointments_loading && <SemanticLoader />}
+            <Segment as={CenterDiv}>
+                <FlexDiv>
+                    <Icon name="user circle" size="massive" />
+                    {doctor && <h1>{doctor.name}</h1>}
+                </FlexDiv>
+                <Divider />
+                <Button.Group>
+                    <Button onClick={()=>navigate(`/doctors/${doctor.id}/edit`)}>Edit</Button>
+                    <Button color="red">Delete</Button>
+                </Button.Group>
+                <Divider />
+            </Segment>
+            <Segment>
+                <h2 style={{textAlign: "center"}}>Current Appointments</h2>
+                {doctor && patients && appointments && 
+                <List divided relaxed size="large" style={{width: "75vw"}}>
+                    {renderAppointments()}
+                </List>}
+                <CenterDiv>
+                    <Button>Add Appointment</Button>
+                    {/* form will go here toggled */}
+                </CenterDiv>
+                <h2 style={{textAlign: "center"}}>Patients</h2>
+                {doctor && patients && appointments && 
+                <List divided relaxed size="large" style={{width: "75vw"}}>
+                {renderPatients()}
+                </List>}
+            </Segment>
         </DoctorsContainer>
     );
 };
